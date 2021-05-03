f3cd75c817c82301e08ceefc056d8da5274f8625
Shaqeeb Momen <shaqeebmomen@users.noreply.github.com>
2021-05-03 17:43:20 -0400

    Initial Setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="button is-twitter">Button</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+require('@/assets/sass/main.scss');
+
+const app = createApp(App).mount('#app')
